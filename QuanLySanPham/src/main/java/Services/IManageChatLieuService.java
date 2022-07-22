2fa0f18a504d058310622b7bb959dc8f2ad0c442
CuongSK <93077164+CuongSK@users.noreply.github.com>
2022-07-22 13:43:53 +0700

    cuongdt_chatLieu

---
+++ QuanLySanPham/src/main/java/Services/IManageChatLieuService.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Services;
+
+import DomainModels.ChatLieu;
+import ViewsModel.ChatLieuModel;
+import java.util.List;
+
+/**
+ *
+ * @author dytc0
+ */
+public interface IManageChatLieuService {
+    List<ChatLieuModel> getProducts(int position, int pageSize);
+    
+    ChatLieuModel insert(ChatLieuModel chatLieu);
+    ChatLieuModel update(ChatLieuModel chatLieu);
+    
+}
